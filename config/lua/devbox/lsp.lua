0dbf44cde227f8c5a1bd12958f15e0ca7061b93e
Robert Timm <mail@rtti.de>
2022-09-22 21:54:29 +0200

    disable java debug for now

--- config/lua/devbox/lsp.lua
+++ config/lua/devbox/lsp.lua
@@ -359,11 +359,11 @@ function M.init_java_lsp()
     'gradlew'
   })
 
-  config.init_options = {
-    bundles = {
-      vim.fn.glob("/home/devbox/.local/lib/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar")
-    }
-  }
+  -- config.init_options = {
+  --   bundles = {
+  --     vim.fn.glob("/home/devbox/.local/lib/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar")
+  --   }
+  -- }
 
   jdtls.start_or_attach(config)
 
