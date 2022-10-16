79904bcfed97cc612fb2d7c935d6db36ffb27bba
Robert Timm <mail@rtti.de>
2022-10-16 21:28:14 +0200

    use update_capabilities

--- config/lua/devbox/lsp.lua
+++ config/lua/devbox/lsp.lua
@@ -129,7 +129,8 @@ local function on_attach(client, bufnr)
 end
 
 local function make_config()
-  local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
   return {
     capabilities = capabilities,
