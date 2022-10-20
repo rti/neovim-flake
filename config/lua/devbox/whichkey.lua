a0e08643d84155a8b016036c147744c2ef3e71dc
Robert Timm <mail@rtti.de>
2022-10-20 22:52:14 +0200

    style: whichkey popup

--- config/lua/devbox/whichkey.lua
+++ config/lua/devbox/whichkey.lua
@@ -44,8 +44,8 @@ function M.setup()
     },
     window = {
       border = "none", -- none, single, double, shadow
-      position = "bottom", -- bottom, top
-      margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+      position = "top", -- bottom, top
+      margin = { 2, 2, 2, 2 }, -- extra window margin [top, right, bottom, left]
       padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
       winblend = 0
     },
@@ -53,7 +53,7 @@ function M.setup()
       height = { min = 4, max = 25 }, -- min and max height of the columns
       width = { min = 20, max = 50 }, -- min and max width of the columns
       spacing = 3, -- spacing between columns
-      align = "left", -- align columns left, center or right
+      align = "center", -- align columns left, center or right
     },
     ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
     hidden = { "<silent>", "<cmd>", "<Cmd>", "<CMD>", "<CR>", "call", "lua", "^:", "^ "}, -- hide mapping boilerplate
