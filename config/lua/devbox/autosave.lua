f76f6d025808fb0aac69cbd686dbdc4b77ef2aef
Robert Timm <mail@rtti.de>
2022-10-20 21:01:01 +0200

    style: code format

--- config/lua/devbox/autosave.lua
+++ config/lua/devbox/autosave.lua
@@ -9,12 +9,12 @@ function M.setup()
     execution_message = {
       message = function() -- message to print on save
         -- return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-        return ("")
+        return ""
       end,
       dim = 0.18, -- dim the color of `message`
       cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
     },
-    trigger_events = {"InsertLeave", "TextChanged"}, -- vim events that trigger auto-save. See :h events
+    trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
     -- function that determines whether to save the current buffer or not
     -- return true: if buffer is ok to be saved
     -- return false: if it's not ok to be saved
@@ -22,9 +22,7 @@ function M.setup()
       local fn = vim.fn
       local utils = require("auto-save.utils.data")
 
-      if
-        fn.getbufvar(buf, "&modifiable") == 1 and
-        utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+      if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
         return true -- met condition(s), can save
       end
       return false -- can't save
@@ -37,8 +35,8 @@ function M.setup()
       disabling = nil, -- ran when disabling auto-save
       before_asserting_save = nil, -- ran before checking `condition`
       before_saving = nil, -- ran before doing the actual save
-      after_saving = nil -- ran after doing the actual save
-    }
+      after_saving = nil, -- ran after doing the actual save
+    },
   })
 end
 
