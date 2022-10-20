e752958096183567de0aca94ab74be006931f7b3
Robert Timm <mail@rtti.de>
2022-10-20 23:15:48 +0200

    chore: noice file split

---
+++ config/lua/devbox/border.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+M.border = {
+  { "🭽", "FloatBorder" },
+  { "▔", "FloatBorder" },
+  { "🭾", "FloatBorder" },
+  { "▕", "FloatBorder" },
+  { "🭿", "FloatBorder" },
+  { "▁", "FloatBorder" },
+  { "🭼", "FloatBorder" },
+  { "▏", "FloatBorder" },
+}
+
+M.borderchars = { '▔', '▕', '▁', '▏', '🭽', '🭾', '🭿', '🭼' }
+
+return M
